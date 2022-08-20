9f6837845f8f9ad459d3812b6df25699f4a9f037
danaRyu <dana40426@gmail.com>
2022-08-20 16:41:19 +0900

    feat: JPA Repository 적용

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,22 +1,30 @@
 package hello.itemservice.domain;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //@Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
-
-    public Item() {
-    }
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public Item() {
+
     }
 }
